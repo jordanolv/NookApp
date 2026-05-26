ab86fa387609d76011ee4548cb53cfa52758ddb8
Jordan <olivier.jordan21@gmail.com>
2026-05-26 21:06:15 +0200

    feat(api): auth, mailer and realtime tweaks

--- apps/api/src/mailer/mailer.service.ts
+++ apps/api/src/mailer/mailer.service.ts
@@ -1,6 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { MAILER_DRIVER, type MailerDriver, type SendMailInput } from './mailer.types';
 import { renderVerifyEmail, type VerifyEmailParams } from './templates/verify-email.template';
+import {
+  renderResetPasswordEmail,
+  type ResetPasswordParams,
+} from './templates/reset-password.template';
 
 @Injectable()
 export class MailerService {
@@ -12,6 +16,11 @@ export class MailerService {
 
   sendVerificationEmail(to: string, params: VerifyEmailParams): Promise<void> {
     const { subject, html, text } = renderVerifyEmail(params);
+    return this.driver.send({ to, subject, html, text });
+  }
+
+  sendResetPasswordEmail(to: string, params: ResetPasswordParams): Promise<void> {
+    const { subject, html, text } = renderResetPasswordEmail(params);
     return this.driver.send({ to, subject, html, text });
   }
 }
